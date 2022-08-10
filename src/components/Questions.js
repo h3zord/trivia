bd4e726fa1243f48e232268ca66ef838c05ea07e
Lucas <lucaschmain@gmail.com>
2022-08-10 13:13:54 -0300

    [GROUP21] - completing requirement 08

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
-// import randomArray from '../tests/helpers/randomArray';
 
 class Question extends React.Component {
   // handleAnswerSubmit = ({ target }) => {

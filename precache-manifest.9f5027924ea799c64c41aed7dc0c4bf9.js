23648f5ca53d91af94f707379b70f08e6602bb88
Lucas <lucaschmain@gmail.com>
2023-01-15 21:40:11 -0300

    Updates

---
+++ precache-manifest.9f5027924ea799c64c41aed7dc0c4bf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a48cbd7caac861c375c66168f126a64",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "2b048a50aee9cd626bcb",
+    "url": "/trivia/static/css/main.74e6cc17.chunk.css"
+  },
+  {
+    "revision": "39cb11507f025e3a5feb",
+    "url": "/trivia/static/js/2.6d0904c4.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/trivia/static/js/2.6d0904c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b048a50aee9cd626bcb",
+    "url": "/trivia/static/js/main.3dc513d6.chunk.js"
+  },
+  {
+    "revision": "13da866554f636ecdfac",
+    "url": "/trivia/static/js/runtime-main.62eca463.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia/static/media/trivia.466d153e.png"
+  }
+]);
